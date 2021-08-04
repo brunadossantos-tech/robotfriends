05836a2f5b065a3818bbce042d50c983e4f60e1d
bfsantos <brunafgdossantos@gmail.com>
2021-08-04 22:26:03 +0100

    small component changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-          <h1>RoboFriends</h1>
-          <SearchBox />
-          <CardList robots={robots}/>  
-        </div>
-        
+class App extends Component {
+    constructor (){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onsearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange={this.onsearchChange}/>
+            <CardList robots={filteredRobots} />  
+            </div>   
     );
+  }
 }
+
 
 export default App;
